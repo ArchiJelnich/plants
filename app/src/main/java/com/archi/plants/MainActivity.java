d10b04e081eb1155c92fe5078f56d1910e6a7252
Archi <artybelov@gmail.com>
2022-02-03 20:58:00 +0300

    initial

---
+++ app/src/main/java/com/archi/plants/MainActivity.java
@@ -0,0 +1,15 @@
+package com.archi.plants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
